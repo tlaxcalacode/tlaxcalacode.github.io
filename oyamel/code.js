4eed1c067e1cc71cd08fc56d1b8e12ed78545158
tlaxcalacode <137142204+tlaxcalacode@users.noreply.github.com>
2023-06-22 23:25:50 -0600

    Update code.js

--- oyamel/code.js
+++ oyamel/code.js
@@ -1,11 +1,64 @@
 const prices = {
    extra: 15,
 }
+const orden = {
+   array: [],
+   remover: (val)=> orden.array = orden.array.filter(arr => arr.ide != val),
+   tam: () => orden.array.length,
+   get: () => orden.array,
+   set: (v) => { orden.array.push(v) },
+   component: ({ide, name, ingr, price }) => {
+      return `
+      <div class="orden-con" data-ide="${ide}">
+         <div class="orden-info">
+            <h2>${name}</h2>
+            <p>${ingr}</p>
+         </div>
+         <div class="orden-price">
+            <b>$${price}</b>
+         </div>
+         <div class="orden-icon">
+            <span class="icon">
+               <svg stroke="currentColor" fill="currentColor" stroke-width="0" version="1.1" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M8 0c-4.418 0-8 3.582-8 8s3.582 8 8 8 8-3.582 8-8-3.582-8-8-8zM8 14.5c-3.59 0-6.5-2.91-6.5-6.5s2.91-6.5 6.5-6.5 6.5 2.91 6.5 6.5-2.91 6.5-6.5 6.5z"></path><path d="M10.5 4l-2.5 2.5-2.5-2.5-1.5 1.5 2.5 2.5-2.5 2.5 1.5 1.5 2.5-2.5 2.5 2.5 1.5-1.5-2.5-2.5 2.5-2.5z"></path></svg>
+            </span>
+         </div>
+      </div>
+   `}
+}
+
+
 /********************* FUNCIONES DEL DOM **********************/
-const funciones = {
+const fdom = {
+   agreCarrito: () => {
+      document.getElementById("agregarAlCarrito").addEventListener("click", () => {
+         let especificaciones = "";
+         document.querySelectorAll("#pedirBurguer form nav").forEach(nav => {
+            nav.querySelectorAll("input").forEach(input => {
+               if (input.checked) {
+                  especificaciones += `${input.value},`
+               }
+            })
+         })
+         let ide;
+         try {
+            ide = (orden.get()[orden.tam()-1].ide)+1
+         } catch (error) {
+            ide = 1
+         }
+         orden.set({
+            ide: ide,
+            ingr: especificaciones,
+            name: pedirBurguer.querySelector("h2").textContent,
+            price: pedirBurguer.querySelector("h4 b").textContent
+         })
+         console.log(orden.get());
+         fdom.pedirBurguer(false)
+      })
+   },
    pedirBurguer: (bol) => {
-      document.getElementById("pedirBurguer").style.top = bol ? '0' : '-100vh'
-      document.getElementById("pedirBurguer").innerHTML = `
+      const pedirBurguer = document.getElementById("pedirBurguer")
+      pedirBurguer.style.top = bol ? '0' : '-100vh'
+      pedirBurguer.innerHTML = `
          <span class="icon" id="pedirBurguerClose"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M400 145.49L366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49z"></path></svg></span>
          <form>
             <img src="./assets/iconburger.png" alt="">
@@ -17,22 +70,14 @@ const funciones = {
             </nav>
             <h3>extras: ($15) c/u</h3>
             <nav>
-               <input type="checkbox" id="chilchorra" data-extra="15" value="extra chilchorra"><label for="chilchorra">chilchorra</label>
+               <input type="checkbox" id="chischorra" data-extra="15" value="extra chischorra"><label for="chischorra">chilchorra</label>
                <input type="checkbox" id="queso" data-extra="15" value="extra queso"><label for="queso">Queso</label>
             </nav>
             <h4>Precio individual: $ <b>---</b></h4>
             <button data-type="ordena" type="button" id="agregarAlCarrito">Agregar al Carrito</button>
          </form>`
-      document.getElementById("pedirBurguerClose").addEventListener("click", () => funciones.pedirBurguer(false))
-      document.getElementById("agregarAlCarrito").addEventListener("click", e => {
-         document.querySelectorAll("#pedirBurguer form nav").forEach(nav =>{
-            nav.querySelectorAll("input").forEach(input =>{
-               if(input.checked){
-                  console.log(input.value);
-               }
-            })
-         })
-      })
+      document.getElementById("pedirBurguerClose").addEventListener("click", () => fdom.pedirBurguer(false))
+      fdom.agreCarrito()
    },
    agregarDatosPedidoBurguer: ({ price, name }) => {
       const section = document.querySelector("#pedirBurguer form")
@@ -41,7 +86,6 @@ const funciones = {
 
       section.querySelectorAll("nav")[1].addEventListener("input", (e) => {
          const b = section.querySelector("h4 b");
-         console.log(e.target.dataset.extra);
          if (e.target.checked) {
             b.textContent = parseInt(b.textContent) + prices.extra
          } else {
@@ -49,10 +93,10 @@ const funciones = {
          }
       })
    },
-   carrito: (bol)=>{
+   carrito: (bol) => {
       document.getElementById("orden").style.top = bol ? '0' : '-100vh'
    },
-   desplegarCategorias: ()=>{
+   desplegarCategorias: () => {
       document.querySelectorAll(".data-category").forEach(dl => {
          dl.addEventListener("click", () => {
             let padre = dl.parentElement;
@@ -66,20 +110,44 @@ const funciones = {
          })
       })
    },
-   funcionesCarrito: ()=>{
-      document.getElementById("carrito").addEventListener("click",() =>{funciones.carrito(true)})
-      document.getElementById("closeOrden").addEventListener("click",() =>{funciones.carrito(false)})
-
-      document.querySelector("#orden section").addEventListener("click",e=>{
-         if(e.target.classList.contains("icon")){
-            alert("eliminar")
+   rellenarCarrito: (array) => {
+      let total=0;
+      document.querySelector("#orden section nav").innerHTML=""
+      array.map(arr => {
+         document.querySelector("#orden section nav").innerHTML += orden.component(arr);
+         total+=parseInt(arr.price)
+      })
+      document.querySelector("#orden section p b").textContent=total;
+      document.getElementById("ordenar").addEventListener("click", e => {
+         if (total == 0) {
+            alert("Por Favor Ordene Algo")
+            fdom.carrito(false)
+            return 
          }
-         console.log(e.target);
+         console.log("Click");
+      })
+   },
+   funcionesCarrito: () => {
+      document.getElementById("carrito").addEventListener("click", () => { fdom.carrito(true); fdom.rellenarCarrito(orden.get()); })
+      document.getElementById("closeOrden").addEventListener("click", () => { fdom.carrito(false) })
+
+      document.querySelector("#orden section").addEventListener("click", e => {
+         if (e.target.classList.contains("icon")) {
+            const el = e.target.parentElement.parentElement
+            orden.remover(el.dataset.ide)
+            document.querySelector("#orden section nav").removeChild(el)
+
+            let total=0;
+            orden.array.map(arr => total+=parseInt(arr.price))
+            document.querySelector("#orden section p b").textContent=total;
+            console.log(total);
+
+         }
       })
    }
 }
-funciones.desplegarCategorias();
-funciones.funcionesCarrito();
+fdom.desplegarCategorias();
+fdom.funcionesCarrito();
 
 document.querySelector(".data").addEventListener("click", e => {
    if (!e.target.classList.contains("data-platillo")) return
@@ -88,8 +156,8 @@ document.querySelector(".data").addEventListener("click", e => {
       price: e.target.dataset.price,
       name: e.target.dataset.name,
    }
-   funciones.pedirBurguer(true)
-   funciones.agregarDatosPedidoBurguer(pedido)
+   fdom.pedirBurguer(true)
+   fdom.agregarDatosPedidoBurguer(pedido)
 
 })
 
