33f15d7c843d1ea7cfd5a0aa7f3402c72a8a61f9
tlaxcalacode <137142204+tlaxcalacode@users.noreply.github.com>
2023-06-26 13:38:50 -0600

    Update code.js

--- oyamel/code.js
+++ oyamel/code.js
@@ -2,12 +2,31 @@ const prices = {
    extra: 15,
 }
 const orden = {
-   array: [],
-   remover: (val)=> orden.array = orden.array.filter(arr => arr.ide != val),
+   array: [
+      {
+         "ide": 4,
+         "ingr": "",
+         "name": "Hamburguesa Clásica",
+         "price": "45"
+      },
+      {
+         "ide": 5,
+         "ingr": "sin jitomate,sin cebolla,sin chiles,extra chischorra,extra queso,",
+         "name": "Hamburguesa Hawaiana",
+         "price": "90"
+      },
+      {
+         "ide": 6,
+         "ingr": "sin jitomate,sin cebolla,extra chischorra,",
+         "name": "Hamburguesa Clásica",
+         "price": "60"
+      }
+   ],
+   remover: (val) => orden.array = orden.array.filter(arr => arr.ide != val),
    tam: () => orden.array.length,
    get: () => orden.array,
    set: (v) => { orden.array.push(v) },
-   component: ({ide, name, ingr, price }) => {
+   component: ({ ide, name, ingr, price }) => {
       return `
       <div class="orden-con" data-ide="${ide}">
          <div class="orden-info">
@@ -25,10 +44,80 @@ const orden = {
       </div>
    `}
 }
+const form = {
+   value: "",
+   set: (v) => { form.value = v },
+   get: () => { return form.value },
+}
+const pedido = {
+   telefono: 7491101224,
+   name: "",
+   calle: "",
+   numero: "",
+   colonia: "",
+   referencias: "",
+   maps: () => { return `https://www.google.com.mx/maps/search/calle+${pedido.calle}+${pedido.numero},+${pedido.colonia}+Cd+Nanacamilpa+Tlax` },
+   orden: [],
+   metodo: "",
+   total: "",
+   mensaje: () => {
+      let msj = `Hola me gustaria ordenar: ${orden.array.map(pe => {
+         return ` ${pe.name.toUpperCase()}, ${pe.ingr} serian $${pe.price}`
+      })}. A nombre de ${pedido.name.toUpperCase()}`
 
-
+      return msj.replaceAll(" ", "%20").replaceAll(",", "%2C").replaceAll(":", "%3A").replaceAll("$", "%24")
+   },
+   link: () => {
+      return `https://api.whatsapp.com/send?phone=+52${pedido.telefono}&text=${pedido.mensaje()}`
+   }
+}
 /********************* FUNCIONES DEL DOM **********************/
 const fdom = {
+   EjecutarOrdenARestaurante: () => {
+      if (orden.get().length < 1) { return alert("Por favor ordene algo") }
+
+      pedido.name = prompt("Por favor Escriba el nombre de quien recibira el pedido")
+
+      console.log(pedido.mensaje());
+      window.open(pedido.link())
+
+   },
+   EjecutarOrdenADomicilio: () => {
+      fdom.domicilio(true)
+      document.getElementById("closeDomicilio").addEventListener("click", () => fdom.domicilio(false))
+      document.getElementById("formDomicilio").addEventListener("submit", e => {
+         e.preventDefault();
+         form.set(e.target)
+
+         const get = form.get()
+
+         pedido.name = get[0].value
+         pedido.calle = get[1].value
+         pedido.numero = get[2].value
+         pedido.colonia = get[3].value
+         pedido.referencias = get[4].value
+
+         console.log(form.get());
+
+         e.target.parentElement.classList.replace("form", "nav")
+
+         document.getElementById("tipoPago").addEventListener("click", t => {
+            if (t.target.tagName.toLowerCase() == "input") {
+               e.target.parentElement.classList.replace("nav", "main")
+               if (t.target.dataset.value == "deposito") {
+                  document.querySelector(`div[data-type="efectivo"]`).style.display = "none"
+               } else {
+                  document.querySelector(`div[data-type="deposito"]`).style.display = "none"
+               }
+            }
+         })
+         // console.log(form.get());
+      })
+
+      // alert("Se requiere acceso a su ubicacion para poder llevarlo a su domicilio")
+      // href="https://www.google.com.mx/maps/search/<?php echo $dirLink; ?>" 
+      // "calle+" . $row['calle'] . "+" . $row['num'] . ",+" . $col . "+Cd+Nanacamilpa+Tlax"
+   },
    agreCarrito: () => {
       document.getElementById("agregarAlCarrito").addEventListener("click", () => {
          let especificaciones = "";
@@ -41,7 +130,7 @@ const fdom = {
          })
          let ide;
          try {
-            ide = (orden.get()[orden.tam()-1].ide)+1
+            ide = (orden.get()[orden.tam() - 1].ide) + 1
          } catch (error) {
             ide = 1
          }
@@ -61,7 +150,7 @@ const fdom = {
       pedirBurguer.innerHTML = `
          <span class="icon" id="pedirBurguerClose"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M400 145.49L366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49z"></path></svg></span>
          <form>
-            <img src="./assets/iconburger.png" alt="">
+            <img src="./assets/burguer.webp" alt="">
             <h2>nombre</h2>
             <nav>
                <input type="checkbox" id="jitomate" value="sin jitomate"><label for="jitomate">Sin Jitomate</label>
@@ -74,7 +163,7 @@ const fdom = {
                <input type="checkbox" id="queso" data-extra="15" value="extra queso"><label for="queso">Queso</label>
             </nav>
             <h4>Precio individual: $ <b>---</b></h4>
-            <button data-type="ordena" type="button" id="agregarAlCarrito">Agregar al Carrito</button>
+            <button data-type="ordena" type="button" id="agregarAlCarrito" class="button">Agregar al Carrito</button>
          </form>`
       document.getElementById("pedirBurguerClose").addEventListener("click", () => fdom.pedirBurguer(false))
       fdom.agreCarrito()
@@ -93,8 +182,47 @@ const fdom = {
          }
       })
    },
+   pedirAlitas: (bol)=>{
+      const pedirBurguer = document.getElementById("pedirBurguer")
+      pedirBurguer.style.top = bol ? '0' : '-100vh'
+      pedirBurguer.innerHTML = `
+         <span class="icon" id="pedirBurguerClose"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M400 145.49L366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49z"></path></svg></span>
+         <form>
+            <img src="./assets/alitas.png" alt="">
+            <h2>nombre</h2>
+            <nav>
+               <input type="checkbox" id="inglesa" value="Con Salsa Inglesa">
+               <label for="inglesa">Con Salsa Inglesa</label>
+               <input type="checkbox" id="magui" value="Con Salsa Magui">
+               <label for="magui">Con Salsa Magui</label>
+               <input type="checkbox" id="valentina" value="Con Salsa Valentina">
+               <label for="valentina">Con Salsa Valentina</label>
+            </nav>
+            <h4>Precio individual: $ <b>---</b></h4>
+            <button data-type="ordena" type="button" id="agregarAlCarrito" class="button">Agregar al Carrito</button>
+         </form>`
+      document.getElementById("pedirBurguerClose").addEventListener("click", () => fdom.pedirBurguer(false))
+      fdom.agreCarrito()
+   },
+   agregarDatosPedidoAlitas: ({ price, name }) => {
+      const section = document.querySelector("#pedirBurguer form")
+      section.querySelector("h2").textContent = name
+      section.querySelector("h4 b").textContent = price
+
+      section.querySelectorAll("nav")[1].addEventListener("input", (e) => {
+         const b = section.querySelector("h4 b");
+         if (e.target.checked) {
+            b.textContent = parseInt(b.textContent) + prices.extra
+         } else {
+            b.textContent = parseInt(b.textContent) - prices.extra
+         }
+      })
+   },
    carrito: (bol) => {
       document.getElementById("orden").style.top = bol ? '0' : '-100vh'
+   },
+   domicilio: (bol) => {
+      document.getElementById("domicilio").style.top = bol ? '0' : '-100vh'
    },
    desplegarCategorias: () => {
       document.querySelectorAll(".data-category").forEach(dl => {
@@ -111,20 +239,21 @@ const fdom = {
       })
    },
    rellenarCarrito: (array) => {
-      let total=0;
-      document.querySelector("#orden section nav").innerHTML=""
+      let total = 0;
+      document.querySelector("#orden section nav").innerHTML = ""
       array.map(arr => {
          document.querySelector("#orden section nav").innerHTML += orden.component(arr);
-         total+=parseInt(arr.price)
+         total += parseInt(arr.price)
       })
-      document.querySelector("#orden section p b").textContent=total;
+      document.querySelector("#orden section p b").textContent = total;
       document.getElementById("ordenar").addEventListener("click", e => {
          if (total == 0) {
             alert("Por Favor Ordene Algo")
             fdom.carrito(false)
-            return 
+            return
          }
-         console.log("Click");
+         // fdom.EjecutarOrdenADomicilio();
+         fdom.EjecutarOrdenARestaurante();
       })
    },
    funcionesCarrito: () => {
@@ -137,13 +266,30 @@ const fdom = {
             orden.remover(el.dataset.ide)
             document.querySelector("#orden section nav").removeChild(el)
 
-            let total=0;
-            orden.array.map(arr => total+=parseInt(arr.price))
-            document.querySelector("#orden section p b").textContent=total;
+            let total = 0;
+            orden.array.map(arr => total += parseInt(arr.price))
+            document.querySelector("#orden section p b").textContent = total;
             console.log(total);
 
          }
       })
+   },
+   ordenar: e => {
+      console.log(e.target.dataset.type);
+      if (e.target.dataset.type == "burguer") {
+         fdom.pedirBurguer(true)
+         fdom.agregarDatosPedidoBurguer({
+            price: e.target.dataset.price,
+            name: e.target.dataset.name,
+         })
+      }
+      if (e.target.dataset.type == "alitas") {
+         fdom.pedirAlitas(true)
+         fdom.agregarDatosPedidoAlitas({
+            price: e.target.dataset.price,
+            name: e.target.dataset.name,
+         })
+      }
    }
 }
 fdom.desplegarCategorias();
@@ -152,12 +298,9 @@ fdom.funcionesCarrito();
 document.querySelector(".data").addEventListener("click", e => {
    if (!e.target.classList.contains("data-platillo")) return
 
-   const pedido = {
-      price: e.target.dataset.price,
-      name: e.target.dataset.name,
-   }
-   fdom.pedirBurguer(true)
-   fdom.agregarDatosPedidoBurguer(pedido)
+   fdom.ordenar(e)
+
+
 
 })
 
